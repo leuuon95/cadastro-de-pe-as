53325fb588c6f8cdb06d9279aa50613a5b72dcfc
Lennon Fonseca <lennonfonsecaa@gmail.com>
2022-06-11 22:17:40 -0300

    adicionar projeto

---
+++ inicio.js
@@ -0,0 +1,21 @@
+let peso = 10;
+let Qtdpecas = 9;
+let nomepecas = "bt";
+
+if (peso >= 100){
+    console.log ("pode cadastrar");
+} else {
+    console.log (" acesso negado");
+}
+
+if (Qtdpecas >= 10){
+    console.log ("nao tem capacidade suficiente");
+} else {
+    console.log ("possui capacidade");
+}
+
+if (nomepecas. length < 3){
+    console.log ("mensagem de erro");
+} else {
+    console.log ("nome peca elegivel");
+}
